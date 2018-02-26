f180a104eea7e61c46c43532a2460a487d7824ca
Spencer Brown <spencerb@warpmail.net>
2018-02-26 16:59:32 -0500

    Add card class inheriting from javafx ImageView.

---
+++ src/Card.java
@@ -0,0 +1,56 @@
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+
+public class Card extends ImageView
+{
+    public Card()
+    {
+
+    }
+
+    public Card(Suit suit, Rank rank)
+    {
+        m_texture = getImageForEnum(suit, rank);
+        setImage(m_texture);
+    }
+
+    private Image getImageForEnum(Suit suit, Rank rank)
+    {
+        String imageFileName = getRankFileName(rank) + "_of_" +
+                getSuitFileName(suit) + ".png";
+        return new Image(imageFileName, 167, 242, false, false);
+    }
+
+    private String getSuitFileName(Suit suit)
+    {
+        switch (suit) {
+            case SPADE: return "spades";
+            case DIAMOND: return "diamonds";
+            case CLUB: return "clubs";
+            case HEART: return "hearts";
+        }
+        return "NA";
+    }
+
+    private String getRankFileName(Rank rank)
+    {
+        switch (rank) {
+            case ACE: return "ace";
+            case TWO: return "2";
+            case THREE: return "3";
+            case FOUR: return "4";
+            case FIVE: return "5";
+            case SIX: return "6";
+            case SEVEN: return "7";
+            case EIGHT: return "8";
+            case NINE: return "9";
+            case TEN: return "10";
+            case JACK: return "jack";
+            case QUEEN: return "queen";
+            case KING: return "king";
+        }
+        return "NA";
+    }
+
+    private Image m_texture;
+}
