ef159d79d4bb81595ccc40d271654ed08b7ab4f8
Spencer Brown <spencerb@warpmail.net>
2018-02-26 17:00:17 -0500

    Add Game class. To launch application and test Card class by creating hardcoded Card instance.

---
+++ src/Game.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+
+public class Game extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        Card card = new Card(Suit.HEART, Rank.TEN);
+        Group root = new Group(card);
+
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setTitle("PISHTI");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
