0adcf7934b176c305ab937696c845899dd749218
Spencer Brown <spencerb@warpmail.net>
2018-02-28 13:12:18 -0500

    Implement Deck class

--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
 	
 	//contains deck array
+    public Deck()
+    {
+    }
+
+    public Deck(ArrayList<Card> cards)
+    {
+        m_cards = cards;
+    }
+
+    public void shuffle()
+    {
+        Collections.shuffle(m_cards);
+    }
+
+    public void addCard(Card card)
+    {
+        m_cards.add(card);
+    }
+
+    public void removeCard(Card card) {
+        m_cards.remove(card);
+    }
+
+    public Card draw()
+    {
+        Card drawnCard =  m_cards.get(m_cards.size() - 1);
+        m_cards.remove(drawnCard);
+        return drawnCard;
+    }
+
+    private ArrayList<Card> m_cards;
 
 }
