fbc74768a9fb213579f64b5aa10d27df0a031623
Spencer Brown <spencerb@warpmail.net>
2018-03-19 10:40:49 -0400

    Clean up remains of "Brainstorming", about enforcing turn order. It can be handled later, if its actually a realistic issue its definitely an edge case.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -30,7 +30,6 @@ public class GameBoard extends BorderPane implements ActionObserver
         m_deck = new Deck();
         m_pile = new Pile();
         m_playerQueue = new ArrayBlockingQueue<Player>(numberOfPlayers);
-        m_turnOverflow = new ArrayList<>();
         m_humanPlayer = new HumanPlayer();
 
         m_playerQueue.add(m_humanPlayer);
@@ -72,37 +71,6 @@ public class GameBoard extends BorderPane implements ActionObserver
         }
     }
 
-    @Override
-    public <T> void onActionNotification(T object, Action action)
-    {
-        if (action == Action.CARDPLAYED) { // object is Players.Player that played card
-            assert(object instanceof Player); // for ActionHandlers.Action.CARDPLAYED object should always be of type Players.Player
-            Player player = (Player) object;
-            Player peek = m_playerQueue.peek();
-            if (player == m_playerQueue.peek()) {
-                m_playerQueue.remove(); // remove player who just played card from front of queue...
-                m_playerQueue.add(player); // and add back at end of queue
-                cardPlayed(player, player.getNextQueuedCard());
-
-                processOverflow();
-            } else { // if player tried to play card out of order
-                // add player to overflow so it can be processed by processOverflow, next time this function is called.
-                if (!m_turnOverflow.contains(player)) {
-                    m_turnOverflow.add(player);
-                }
-            }
-        }
-    }
-
-    private void processOverflow()
-    {
-        if (m_turnOverflow.isEmpty())
-            return;
-        for (Player player : m_turnOverflow) {
-
-        }
-    }
-
     // card was played.  remove it from the hand of the player that played it, and notify other players.
     private void cardPlayed(Player playerOfCard, Card card)
     {
@@ -123,9 +91,25 @@ public class GameBoard extends BorderPane implements ActionObserver
 
     }
 
+    @Override
+    public <T> void onActionNotification(T object, Action action)
+    {
+        if (action == Action.CARDPLAYED) { // object is Players.Player that played card
+            assert(object instanceof Player); // for ActionHandlers.Action.CARDPLAYED object should always be of type Players.Player
+            Player player = (Player) object;
+            Player peek = m_playerQueue.peek();
+            if (player == m_playerQueue.peek()) {
+                m_playerQueue.remove(); // remove player who just played card from front of queue...
+                m_playerQueue.add(player); // and add back at end of queue
+                cardPlayed(player, player.getNextQueuedCard());
+            } else { // handle: player tried to play card out of turn
+
+            }
+        }
+    }
+
     private HumanPlayer m_humanPlayer;
     private Queue<Player> m_playerQueue; // holds players - In queue to track and enforce turn order.
-    private ArrayList<Player> m_turnOverflow; //remembers players who have tried to play card out of order.
     private Deck m_deck;
     private Pile m_pile;
 }
