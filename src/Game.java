0e4b3b51f3e46d41c44088fcf3031d78500ff741
Spencer Brown <spencerb@warpmail.net>
2018-03-29 21:16:11 -0400

    Add application icon.

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -14,6 +15,7 @@ public class Game extends Application
         Scene scene = new Scene(board);
 
         primaryStage.setTitle("PISHTI");
+        primaryStage.getIcons().add(new Image("ace_of_clubs.png"));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
